e061682fb0f5c222ce7f38398bf0c4cd1fe21a66
Arslan Ali <ali.arslan319@gmail.com>
2019-04-14 23:57:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -207,6 +207,26 @@ def player_stats(name)
   end
 end
 
+def find_player_with_longest_shoe
+  largest_shoe_size = nil
+  player_with_largest_shoe = nil
+    game_hash.each do |location, team_data|
+    team_data[:players].each do |player_name, player_stats|
+      if largest_shoe_size == nil || player_stats[:shoe] > largest_shoe_size
+        largest_shoe_size = player_stats[:shoe]
+        player_with_largest_shoe = player_name
+     end
+    end
+  end
+  player_with_largest_shoe
+end
+
+
+
+
+
+
+
 def big_shoe_rebounds
   player_with_largest_shoe = find_player_with_longest_shoe
   game_hash.each do |location, team_data|
